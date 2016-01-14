c5a78f17e9ccad7b208d3933ad40035f9dff2bd8
Nikola-K <nikolak@outlook.com>
2016-01-14 22:46:03 +0100

    added user admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import Person
 
+
 @admin.register(Person)
-class PersonAdminModel(admin.ModelAdmin):
-    fields = ['username', 'last_login']
+class PersonAdminModel(UserAdmin):
+    pass
