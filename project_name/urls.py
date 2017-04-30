b1fffc83a0086db478211118538c8bea0d07761d
Nikola-K <nikolak@outlook.com>
2017-04-30 20:46:18 +0200

    Add debug toolbar urls if in debug mode

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import include, url
-from django.contrib import admin
 from django.conf import settings
-from django.views.generic import TemplateView
+from django.conf.urls import include, url
 from django.conf.urls.static import static
+from django.contrib import admin
 from django.views import defaults as default_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(settings.ADMIN_URL, include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
-    url(r'^users/', include('users.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  url(settings.ADMIN_URL, include(admin.site.urls)),
+                  url(r'^$', TemplateView.as_view(template_name='index.html'), name="home"),
+                  url(r'^users/', include('users.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
+    import debug_toolbar
+
     # This allows the error pages to be debugged during development, just visit
     # these url in browser to see how these error pages look like.
     urlpatterns += [
@@ -21,4 +23,8 @@ if settings.DEBUG:
         url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception("Permission Denied")}),
         url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception("Page not Found")}),
         url(r'^500/$', default_views.server_error),
+    ]
+
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
     ]
